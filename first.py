708757cd6e4a8d5153f4e6748b71aa425a9af7db
umeshmehta6551 <148803637+umeshmehta6551@users.noreply.github.com>
2024-08-29 21:48:49 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,7 @@
+print("Hello This is My Firsrt Day of Python :")
+
+# this is used for create a comment in the code 
+
+""" this is used for a multiline  comments in the code   we can write multiline comments """
+
+# if we want to put a stirng in the double cot we can use it by \" Good Boy\ "
