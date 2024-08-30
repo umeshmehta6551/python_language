1402ba38454944d18b17eefb5da8b4a205ef63de
umeshmehta6551 <148803637+umeshmehta6551@users.noreply.github.com>
2024-08-30 22:05:56 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,24 @@
+a=10
+a1=20
+b=True
+c="umesh"
+d=None
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(a+a1)
+
+
+# list 
+list1=[1,3,4,[-3,-4,-5],["umesh"]] # list is changable    
+print(list1)
+
+#tuple
+tuple =(("umesh","sparrow",("shubham","vaibhav")))
+print((tuple))
+# tuple is not changable and it immutable 
+
+#dictionary
+dic={ "Name","shakshi","age",20}
+print((dic))
