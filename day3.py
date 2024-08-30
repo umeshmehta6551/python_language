4a2178b931e25f0637ff303f9beb7df3995fd757
umeshmehta6551 <148803637+umeshmehta6551@users.noreply.github.com>
2024-08-30 22:16:02 +0530

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,16 @@
+# user input 
+# by using inout function we can take inoput by user 
+
+
+
+# a=input(" Enter Your Name :")
+# print("My Name is :",a)
+
+#perform basic Arithemetic operations 
+X=input("Enter The first Number :")
+Y=input("Enter the Second Number :")
+
+print("the sum of X  And Y ",(int(X)+(int)(Y)))
+print("the sub of X  And Y ",(int(X)-(int)(Y)))
+print("the mul of X  And Y ",(int(X)*(int)(Y)))
+print("the div of X  And Y ",(int(X)/(int)(Y)))
